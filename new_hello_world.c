5b36fb5ce23038a090053a7c6a179978ec042ebd
CO18344 <42909454+CO18344@users.noreply.github.com>
2019-10-30 17:05:31 +0530

    Create new_hello_world.c
    Hello World with modular programming approach.

---
+++ new_hello_world.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void printHello();      // Function Prototype
+
+void main() //Driver function
+{
+	printHello();
+}
+
+void printHello()       // FUnction Definition
+{
+	printf("Hello world!!!");
+}
